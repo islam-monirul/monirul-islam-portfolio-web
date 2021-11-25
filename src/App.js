b424f4e562db8d8cc1106a26b98dae061f0641dc
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-11-25 16:04:08 +0600

    error occured deleted previous functionality for adding correction

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
-import AuthProvider from "./Context/AuthProvider";
-import Login from "./Pages/Authentication/Login/Login";
 import Homepage from "./Pages/Homepage/Homepage";
-import PrivateRoute from "./Pages/Authentication/PrivateRoute/PrivateRoute";
-import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard/AdminDashboard";
 import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Homepage />
-          </Route>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
 
-          <Route path="/login">
-            <Login />
-          </Route>
-
-          <PrivateRoute path="/adminDashboard">
-            <AdminDashboard />
-          </PrivateRoute>
-
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </AuthProvider>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
