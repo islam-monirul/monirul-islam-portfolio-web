9cbabd49e93b0f7a5fc91393d72b59719d1c06a3
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-11-25 10:49:10 +0600

    private routing and not found page route added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import "./App.css";
 import AuthProvider from "./Context/AuthProvider";
 import Login from "./Pages/Authentication/Login/Login";
 import Homepage from "./Pages/Homepage/Homepage";
+import PrivateRoute from "./Pages/Authentication/PrivateRoute/PrivateRoute";
+import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard/AdminDashboard";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Switch>
-          <Route path="/">
-            <Homepage></Homepage>
+          <Route exact path="/">
+            <Homepage />
           </Route>
 
           <Route path="/login">
-            <Login></Login>
+            <Login />
+          </Route>
+
+          <PrivateRoute path="/adminDashboard">
+            <AdminDashboard />
+          </PrivateRoute>
+
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </BrowserRouter>
