884967feffe9e2b1a32db0d3109396ba6cd2a884
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-11-30 01:24:27 +0600

    contact page updated

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -11,6 +11,29 @@ import {
 } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import "./Contact.css";
+import {
+  FaPhoneSquareAlt,
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaGithub,
+  FaLinkedinIn,
+  FaFacebook,
+} from "react-icons/fa";
+
+const socialLinks = [
+  {
+    name: "github",
+    link: "https://github.com/islam-monirul",
+  },
+  {
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/monirshojib09/",
+  },
+  {
+    name: "fb",
+    link: "https://www.facebook.com/monirshojib09/",
+  },
+];
 
 const Contact = () => {
   const [loading, setLoading] = useState(false);
@@ -44,10 +67,43 @@ const Contact = () => {
   };
 
   return (
-    <Container>
-      <Row>
-        <Col md={6}></Col>
-        <Col md={6}>
+    <Container className="py-5">
+      <Row className="d-flex justify-content-center">
+        <Col md={5} lg={4}>
+          <h4 className="pb-3">Get in touch</h4>
+          <div className="pb-2">
+            <p>
+              <FaPhoneSquareAlt /> &nbsp;
+              <span className="align-middle">+880 152 133 4471</span>
+            </p>
+            <p>
+              <FaEnvelope /> &nbsp;
+              <span className="align-middle">devmonir.uiu.cse@gmail.com</span>
+            </p>
+            <p>
+              <FaMapMarkerAlt /> &nbsp;
+              <span className="align-middle">Dhaka, Bangladesh</span>
+            </p>
+          </div>
+
+          <div className="d-flex gap-2">
+            {socialLinks.map((link) => (
+              <Button
+                className="socialLinkBox"
+                onClick={() => window.open(`${link?.link}`, "_blank")}
+              >
+                {link?.name === "github" ? (
+                  <FaGithub />
+                ) : link?.name === "linkedin" ? (
+                  <FaLinkedinIn />
+                ) : (
+                  <FaFacebook />
+                )}
+              </Button>
+            ))}
+          </div>
+        </Col>
+        <Col md={6} lg={6} className="mt-5 mt-md-0">
           {loading ? (
             <Spinner animation="grow" variant="success" />
           ) : (
